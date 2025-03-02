f94c49b315d27178ffe19faa1569bf8537a9c457
Andrew <achapman125@students.mchenry.edu>
2025-03-02 15:21:11 -0600

    Datatypes assignment

---
+++ datatypes.py
@@ -0,0 +1,20 @@
+# This is a Python program to demonstrate different data types
+
+# Literal
+print("Literal:", 5000)
+
+# Integer
+y = -45
+print("Integer:", y)
+
+# Float
+z = 10
+print("Float:", z)
+
+# String
+name = "Gertrude"
+print("String:", name)
+
+# Boolean
+is_active = False
+print("Boolean:", is_active)
