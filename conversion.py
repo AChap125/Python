2ca5d801c456920af8ecdb0809efaba5e083792f
Andrew <achapman125@students.mchenry.edu>
2025-03-02 15:31:10 -0600

    Conversion of kilograms to pounds

---
+++ conversion.py
@@ -0,0 +1,13 @@
+kg_value_1 = 10
+kg_value_2 = 20
+kg_value_3 = 30
+kg_value_4 = 40
+conversion_factor = 2.20462
+pounds_1 = kg_value_1 * conversion_factor
+pounds_2 = kg_value_2 * conversion_factor
+pounds_3 = kg_value_3 * conversion_factor
+pounds_4 = kg_value_4 * conversion_factor
+print(f"{kg_value_1} kilograms is equal to {pounds_1:.2f} pounds.")
+print(f"{kg_value_2} kilograms is equal to {pounds_2:.2f} pounds.")
+print(f"{kg_value_3} kilograms is equal to {pounds_3:.2f} pounds.")
+print(f"{kg_value_4} kilograms is equal to {pounds_4:.2f} pounds.")
