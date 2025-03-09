dc0971de66140dd5dcc0943d239a971605f473b4
AChap125 <achapman125@students.mchenry.edu>
2025-03-09 13:35:46 -0500

    Add files via upload

---
+++ for loops.py
@@ -0,0 +1,6 @@
+# Use a for loop to iterate through numbers from 1 to 500
+for number in range(1, 501):
+    # Check if the number is divisible by 7 using the modulus operator
+    if number % 7 == 0:
+        # Print the number if it is divisible by 7
+        print(number)
