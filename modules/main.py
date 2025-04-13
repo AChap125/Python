f85081a663875639bfe0efe560cc2ba9da156177
AChap125 <achapman125@students.mchenry.edu>
2025-04-13 18:38:50 -0500

    Add files via upload

---
+++ modules/main.py
@@ -0,0 +1,6 @@
+from math_operations import calculator
+from math_operations import geometry
+
+print("5 + 3 =", calculator.add(5, 3))
+print("10 - 4 =", calculator.subtract(10, 4))
+print("Circle area (r=5):", geometry.circle_area(5))
